aca09231391499ec3c1e6e064ac3ebc4fb1facf0
DESKTOP-600SUA7\alvin <alvinleehonguang@gmail.com>
2018-06-10 23:03:36 +0800

    pop up for wrong location

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController  } from 'ionic-angular';
 import { Http, URLSearchParams } from '@angular/http';
 import 'rxjs/add/operator/map';
 @Component({
@@ -7,41 +7,61 @@ import 'rxjs/add/operator/map';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-start:any;
-destination:any;
-distance:any;
+  start: any;
+  destination: any;
+  distance: any;
   text = {
     "number": "",
     "message": "",
   };
   posts: any;
-  constructor(public navCtrl: NavController, public http: Http) {
- 
+  constructor(public navCtrl: NavController, public http: Http,private alertCtrl: AlertController) {
+
   }
-    sendSMS() {
-      console.log("heelo")
-  
-  
-  
-      let search = new URLSearchParams();
-     // search.append('ID', '95270002');
-      search.append('Password', 'hello123');
-      search.append('Mobile', '6592223123');
-      search.append('Type', 'A');
-      search.append('Message', 'Hello');
-  
-      this.http.post('https://www.commzgate.net/gateway/SendMsg', search).subscribe(res => console.log(res.json.toString()));
-  
-    }
+  sendSMS() {
+    console.log("heelo")
 
-    generateDistance(){
-      this.posts = null;
 
-      this.http.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=km&origins=' +this.start+ '&destinations='+this.destination+'&key=AIzaSyBVXQmKg9DsI69_Vm_cnbhLWzXI8jG2SSA').map(res => res.json()).subscribe(data => {
+
+    let search = new URLSearchParams();
+    // search.append('ID', '95270002');
+    search.append('Password', 'hello123');
+    search.append('Mobile', '6592223123');
+    search.append('Type', 'A');
+    search.append('Message', 'Hello');
+
+    this.http.post('https://www.commzgate.net/gateway/SendMsg', search).subscribe(res => console.log(res.json.toString()));
+
+  }
+
+  generateDistance() {
+    this.posts = null;
+
+    this.http.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=km&origins=' + this.start + '&destinations=' + this.destination + '&key=AIzaSyBVXQmKg9DsI69_Vm_cnbhLWzXI8jG2SSA').map(res => res.json()).subscribe(data => {
+     
+    
+    if (data.rows[0].elements[0].status !==("NOT_FOUND")) {
+
         this.posts = data.rows[0].elements[0].distance.text;
         console.log(this.posts);
-       this.distance = this.posts;
-      })
-   
+
+        this.distance = this.posts;
+      }else{
+        this.showAlert();
+      }
+    })
+
+  }
+
+ 
+    showAlert() {
+      let alert = this.alertCtrl.create({
+        title: 'Please check your location',
+        subTitle: 'Location not found',
+        buttons: ['OK'],
+        cssClass: 'alertCustomCss'
+      });
+      alert.present();
     }
+  
 }
