c265744c55f42c75527403f1a40c046f40ff8e0f
DESKTOP-600SUA7\alvin <alvinleehonguang@gmail.com>
2018-06-10 20:17:04 +0800

    google api working on form
    google api working on form

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -7,19 +7,16 @@ import 'rxjs/add/operator/map';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
+start:any;
+destination:any;
+distance:any;
   text = {
     "number": "",
     "message": "",
   };
   posts: any;
   constructor(public navCtrl: NavController, public http: Http) {
-    this.posts = null;
-
-    this.http.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=km&origins=570158&destinations=nus&key=AIzaSyBVXQmKg9DsI69_Vm_cnbhLWzXI8jG2SSA').map(res => res.json()).subscribe(data => {
-      this.posts = data.rows[0].elements[0].distance.text;
-      console.log(this.posts);
-    })
+ 
   }
     sendSMS() {
       console.log("heelo")
@@ -27,7 +24,7 @@ export class AboutPage {
   
   
       let search = new URLSearchParams();
-      search.append('ID', '95270002');
+     // search.append('ID', '95270002');
       search.append('Password', 'hello123');
       search.append('Mobile', '6592223123');
       search.append('Type', 'A');
@@ -35,5 +32,16 @@ export class AboutPage {
   
       this.http.post('https://www.commzgate.net/gateway/SendMsg', search).subscribe(res => console.log(res.json.toString()));
   
+    }
+
+    generateDistance(){
+      this.posts = null;
+
+      this.http.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=km&origins=' +this.start+ '&destinations='+this.destination+'&key=AIzaSyBVXQmKg9DsI69_Vm_cnbhLWzXI8jG2SSA').map(res => res.json()).subscribe(data => {
+        this.posts = data.rows[0].elements[0].distance.text;
+        console.log(this.posts);
+       this.distance = this.posts;
+      })
+   
     }
 }
